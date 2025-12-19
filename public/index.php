2c70a5465c3311415091e7fc1b5eeb776235a5f0
mouna243 <mounasaoui2108@gmail.com>
2025-12-19 16:27:59 +0100

    folder public

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+// Include header
+include  __DIR__. "/../templates/header.php";
+// Include layaout
+include  __DIR__. "/../templates/layout.php";
+
+include __DIR__."/../config/database.php";
+// Include footer
+include  __DIR__. "/../templates/footer.php";
+
+?>
