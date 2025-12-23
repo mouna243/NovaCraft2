dfb728c5924ea1f304ed70362153f60b37e6fd1e
mouna243 <mounasaoui2108@gmail.com>
2025-12-23 15:17:26 +0100

    l'ajoute de z-index

--- templates/footer.php
+++ templates/footer.php
@@ -2,6 +2,6 @@
     <link rel="stylesheet" href="./src/output.css">
 </head>
 
-<footer class="absolut bottom-0 bg-[#16b7d9] flex justify-center items-center h-[50px] w-[100%] z-0">
+<footer class="absolut bottom-0 bg-[#16b7d9] flex justify-center items-center h-[50px] w-[100%] z-5">
     <p class="z-20">© 2025 DigitalWave Solutions. Tous droits réservés.</p>
 </footer>
