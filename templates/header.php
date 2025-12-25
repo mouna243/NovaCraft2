501afc77838a65a8da27e096aa93e1d914e95e3a
mouna243 <mounasaoui2108@gmail.com>
2025-12-25 03:06:56 +0100

    le cangement des button afficher d'apres situation de user

--- templates/header.php
+++ templates/header.php
@@ -4,9 +4,6 @@
     <title>Nova Craft</title>
     <link rel="icon" href="./asset/logo.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-
-
-
 </head>
 
 <nav class="fixed w-[100%] flex justify-between p-5 bg-purple-300/40 z-50 shadow-lg shadow-[#504f4f65]">
@@ -24,7 +21,8 @@
             class="hover:shadow-lg hover:shadow-white hover:bg-[#16b8d967] hover:text-[20px] hover:text-white p-3 rounded-[20px]">Contact</a>
     </div>
     <div class="flex w-auto gap-2 justify-between ">
-        <?php $situ = false ?>
+        <?php
+        $situ = $_SESSION["situ"] ?? false; ?>
         <?php if ($situ === false): ?>
             <button
                 class="border-[3px] border-white p-2 w-[100px] h-[50px] rounded-[20px] bg-purple-600 text-white hover:bg-purple-800 hover:text-[20px]"><a
@@ -36,15 +34,11 @@
         <?php if ($situ === true): ?>
 
             <img src="./asset/persone.jpg" alt="profil" id="profil" class="w-[50px] h-[50px] rounded-[20px]">
-            <div id="sidebar"></div>
+            <button href="logout" class='border-[3px] border-white p-2 w-[100px] h-[50px] rounded-[20px] bg-purple-600 text-white hover:bg-purple-800'><a href="logout"><i class='fa-solid fa-arrow-right-from-bracket'></i></a></button>
+          
         <?php endif ?>
     </div>
 
 </nav>
-<script>
-    const profil = document.getElementById("profil");
-    profil.addEventListener("click", (){
-        const sidebar = document.getElementById("sidebar");
-        sidebar.innerHTML = "<button class='border-[3px] border-white p-2 w-[100px] h-[50px] rounded-[20px] bg-purple-600 text-white hover:bg-purple-800'><i class='fa-solid fa-arrow-right-from-bracket'></i></button>"
-    })
-</script>
+
+
